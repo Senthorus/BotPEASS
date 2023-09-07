bf97ed892bbd888ecffc4e604e8a162f1c668a93
jerome <jerome.arn@hotmail.fr>
2023-09-07 23:33:56 +0200

    test

--- botpeas.py
+++ botpeas.py
@@ -168,7 +168,7 @@ def send_teams_mesage(cve_data: dict):
     """ Send a message to the teams channel """
 
     teams_url = os.getenv('TEAMS_WEBHOOK_DEV')
-
+    print(os.environ)
     if not teams_url:
         print("TEAMS_WEBHOOK wasn't configured in the secrets!")
         return
